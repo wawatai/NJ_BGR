b5d13ba2e653e2de74fa68d977b5faede250cd3f
wawatai <wataiPublic@gmail.com>
2022-12-14 18:32:14 +0800

    111-12-14

---
+++ js/js.js
@@ -0,0 +1,37 @@
+$(function(){
+    //Swiper
+    var swiper = new Swiper('.swiper', {
+        direction: 'vertical',
+        autoHeight: true,
+        loop: true,
+        autoplay: {
+            delay: 1500,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true,
+        },
+        slidesPerView: 1,
+        // spaceBetween: 30,
+        breakpoints: {
+            // 1600: {
+            //     slidesPerView: 1,
+            //     spaceBetween: 30,
+            // },
+            // 1260: {
+            //     slidesPerView: 1,
+            //     spaceBetween: 30,
+            // },
+            1000: {
+                slidesPerView: 1,
+                spaceBetween: 0,
+            }
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+    });
+})
