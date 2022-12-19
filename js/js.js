a4d97132e7c8c99cfa4118cea28144e76d4b3d26
wawatai <wataiPublic@gmail.com>
2022-12-19 15:32:04 +0800

    111-12-19

--- js/js.js
+++ js/js.js
@@ -34,3 +34,76 @@ $(function(){
         },
     });
 })
+
+$(function(){
+    $(".gameType .top li").click(function(){
+        $(this)
+        .toggleClass("active");
+       
+        $(this).siblings()
+        .removeClass("active");
+
+        var log = $(this).index();
+
+        if( log == 0) 
+        {
+            $(".typeContent.hot")
+            .addClass("display");
+
+            $(".typeContent.hot").siblings()
+            .removeClass("display");
+        }
+
+        if( log == 1) 
+        {
+            $(".typeContent.all")
+            .addClass("display");
+
+            $(".typeContent.all").siblings()
+            .removeClass("display");
+        }
+
+        if(log == 2) 
+        {
+            $(".typeContent.sport")
+            .addClass("display");
+
+            $(".typeContent.sport").siblings()
+            .removeClass("display");
+        }
+
+        if(log == 3) 
+        {
+            $(".typeContent.live")
+            .addClass("display");
+
+            $(".typeContent.live").siblings()
+            .removeClass("display");
+        }
+        if(log == 4) 
+        {
+            $(".typeContent.slot")
+            .addClass("display");
+
+            $(".typeContent.slot").siblings()
+            .removeClass("display");
+        }
+        if(log == 5) 
+        {
+            $(".typeContent.fish")
+            .addClass("display");
+
+            $(".typeContent.fish").siblings()
+            .removeClass("display");
+        }
+        if(log == 6) 
+        {
+            $(".typeContent.board")
+            .addClass("display");
+
+            $(".typeContent.board").siblings()
+            .removeClass("display");
+        }
+    })
+
+})
